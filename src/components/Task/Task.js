e74a7ad9315f7cb66c227c7bbd9d19b350fd1ac2
DanilBezrukov <danil.bezrukov@icloud.com>
2023-05-20 15:05:20 +0300

    Добавил логику установки флажка и вывод даты в Task

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,22 +3,31 @@ import "./Task.css"
 function Task ( { data, index, nameCategory, setTaskStorage } ){
     const {taskTitle, taskText, date, flag} = data
     // console.log("data > ", taskTitle, taskText, date, flag)
-    function taskStatusChange (event) {
-        console.log(event.currentTarget)
-
+    function taskStatusChange () {
         setTaskStorage(data =>{
-            console.log(data[nameCategory][index])
-            console.log(data)
-
-            return data
+            data[nameCategory][index].flag = !flag
+            return {...data}
         })
     }
+
+    function CreatDate() {
+        const srcDate = new Date(date)
+        const day = srcDate.getDate().toString().padStart(2, '0')
+        const month = (srcDate.getMonth() + 1).toString().padStart(2, '0')
+        const hour = srcDate.getHours().toString().padStart(2, '0')
+        const minute = srcDate.getMinutes().toString().padStart(2, '0')
+
+        return (
+            <p>{day}.{month} {hour}:{minute}</p>
+        )
+    }
+
     return (
         <div>
         <input type="checkbox" id={taskTitle} checked={flag} onChange={taskStatusChange}/>
         <p>{taskTitle}</p>
         <p>{taskText}</p>
-        {/* <p>{date}</p> */}
+        <CreatDate/>
 
         </div>
     )
