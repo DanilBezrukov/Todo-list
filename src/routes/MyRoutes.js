7825994aae176fb8e8ddee9d8dc66b57d8f92790
DanilBezrukov <danil.bezrukov@icloud.com>
2023-05-20 13:08:26 +0300

    Добавил логику сохранения и получени состояний в localStorage

--- src/routes/MyRoutes.js
+++ src/routes/MyRoutes.js
@@ -5,31 +5,19 @@ import CreateTaskPage from "../pages/CreateTaskPage/CreateTaskPage";
 import { useEffect, useState } from "react";
 
 function MyRoutes() {
-    const [category, setCategory] = useState([])
-    const [taskStorage, setTaskStorage] = useState({
-        "Личные": [{ taskTitle: 'Учеба', taskText: 'Сдать курсовую', date: "Fri May 19 2023 10:09:56 GMT+0300 (Москва, стандартное время)", flag: false }, { taskTitle: 'Спорт', taskText: 'Выжать от груди 100кг', date: "Fri May 19 2023 10:28:34 GMT+0300 (Москва, стандартное время)", flag: true }],
-    })
-    const [mappingFlag, setMappingFlag] = useState(true)
-    const [sortDate, setSortDate] = useState("new")
-    const [sortCompleted, setSortCompleted] = useState("NotFulfilled")
-
-
-    useEffect(() => {
-        console.log("Монтаж")
-        // setCategory(JSON.parse(localStorage.getItem("category")) ?? [])
-        // setTaskStorage(JSON.parse(localStorage.getItem("taskStorage")) ?? {})
-        // setMappingFlag(JSON.parse(localStorage.getItem("mappingFlag")) ?? true)
-        // setSortDate(localStorage.getItem("sortDate") ?? "new")
-        // setSortCompleted(localStorage.getItem("mappingFlag") ?? "sortCompleted")
-    }, [])
+    const [category, setCategory] = useState(JSON.parse(localStorage.getItem("category")) ?? [])
+    const [taskStorage, setTaskStorage] = useState(JSON.parse(localStorage.getItem("taskStorage")) ?? {})
+    const [mappingFlag, setMappingFlag] = useState(JSON.parse(localStorage.getItem("mappingFlag")) ?? true)
+    const [sortDate, setSortDate] = useState(localStorage.getItem("sortDate") ?? "new")
+    const [sortCompleted, setSortCompleted] = useState(localStorage.getItem("sortCompleted") ?? "NotFulfilled")
 
     useEffect(() => {
         console.log("Рендор")
-        // localStorage.setItem("category", JSON.stringify(category));
-        // localStorage.setItem("taskStorage", JSON.stringify(taskStorage));
-        // localStorage.setItem("mappingFlag", JSON.stringify(mappingFlag));
-        // localStorage.setItem("sortDate", sortDate);
-        // localStorage.setItem("sortCompleted", sortCompleted);
+        localStorage.setItem("category", JSON.stringify(category));
+        localStorage.setItem("taskStorage", JSON.stringify(taskStorage));
+        localStorage.setItem("mappingFlag", JSON.stringify(mappingFlag));
+        localStorage.setItem("sortDate", sortDate);
+        localStorage.setItem("sortCompleted", sortCompleted);
     }, [category, taskStorage, mappingFlag, sortDate, sortCompleted])
 
     return (
